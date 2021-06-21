77f49fbdd96ca15cd029864262821ef95c9b150f
Codecov Devops <devops@codecov.local>
2021-06-20 18:10:43 -0700

    adjusted test

--- cypress/integration/03_date.spec.js
+++ cypress/integration/03_date.spec.js
@@ -5,9 +5,9 @@ context('Date', () => {
     cy.clock(new Date(104907600000));
     cy.visit(Cypress.env('baseUrl'));
 
-    cy.getByTestId('day').should('have.text', '29');
+    cy.getByTestId('day').should('have.text', '28');
     cy.getByTestId('year').should('have.text', '1973');
     cy.getByTestId('month').should('have.text', 'Apr');
-    cy.getByTestId('week-day').should('have.text', 'Sunday');
+    cy.getByTestId('week-day').should('have.text', 'Saturday');
   });
 });
